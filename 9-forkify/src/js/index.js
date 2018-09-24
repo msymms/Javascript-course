d05aa974dcdb0bf8e8464be3f686f78c84b44729
marksymms <msymms@gmail.copm>
2018-09-24 08:34:08 -0500

    still setting up work env

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(`I imported ${x} from another module`);
+const a = 23;
+console.log(`I imported ${x} from another module from module test.js!  I am no longer ${a}`);
